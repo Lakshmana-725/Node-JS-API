6b599101c936a9d17b3c084efc142a129b0b4332
Lakshman <lakshmanp@gmail.com>
2024-07-08 21:20:19 +0530

    Updated API's With Specific ID Data

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mysql = require("mysql2");
 const app = express();
 const port = 3001; // Choose any available port
 
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Create MySQL connection
 const connection = mysql.createConnection({
   host: "localhost", // Remove 'http://' and just use 'localhost'
@@ -36,6 +40,28 @@ app.get("/books", (req, res) => {
   });
 });
 
+// Define API endpoint to get a specific book by ID
+app.get('/books/:id', (req, res) => {
+  // console.log("🚀 ~ file: index.js:45 ~ req:-- ", req)
+  const bookId = req.params.id;
+  const query = `SELECT * FROM books WHERE book_id = ?`;
+
+  connection.query(query, [bookId], (err, results) => {
+    if (err) {
+      console.error('Error fetching data from database:', err);
+      res.status(500).send('Error fetching data');
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send('Book not found');
+      return;
+    }
+
+    res.json(results[0]); // Assuming there's only one book with the given ID
+  });
+});
+
 // Get all authors
 
 app.get("/authors", (req, res) => {
@@ -46,6 +72,28 @@ app.get("/authors", (req, res) => {
     } else {
       res.json(results);
     }
+  });
+});
+
+// Define API endpoint to get a specific authors by ID
+app.get("/authors/:id", (req, res) => {
+  // console.log("🚀 ~ file: index.js:80 ~ req:-- ", req)
+  const authorId = req.params.id;
+  const query = `SELECT * FROM authors WHERE author_id = ?`;
+
+  connection.query(query, [authorId], (err, results) => {
+    if (err) {
+      console.error("Error fetching data from database:", err);
+      res.status(500).send("Error fetching data");
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send("author not found");
+      return;
+    }
+
+    res.json(results[0]); // Assuming there's only one author with the given ID
   });
 });
 
@@ -62,6 +110,28 @@ app.get("/customers", (req, res) => {
   });
 });
 
+// Define API endpoint to get a specific customers by ID
+app.get("/customers/:id", (req, res) => {
+// console.log("🚀 ~ file: index.js:115 ~ req:-- ", req)
+  const customersId = req.params.id;
+  const query = `SELECT * FROM customers WHERE customer_id = ?`;
+
+  connection.query(query, [customersId], (err, results) => {
+    if (err) {
+      console.error("Error fetching data from database:", err);
+      res.status(500).send("Error fetching data");
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send("Book not found");
+      return;
+    }
+
+    res.json(results[0]); // Assuming there's only one customers with the given ID
+  });
+});
+
 // Get all orders
 
 app.get("/orders", (req, res) => {
@@ -72,6 +142,28 @@ app.get("/orders", (req, res) => {
     } else {
       res.json(results);
     }
+  });
+});
+
+// Define API endpoint to get a specific orders by ID
+app.get("/orders/:id", (req, res) => {
+  // console.log("🚀 ~ file: index.js:150 ~ req:-- ", req)
+  const ordersId = req.params.id;
+  const query = `SELECT * FROM orders WHERE order_id = ?`;
+
+  connection.query(query, [ordersId], (err, results) => {
+    if (err) {
+      console.error("Error fetching data from database:", err);
+      res.status(500).send("Error fetching data");
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).send("Book not found");
+      return;
+    }
+
+    res.json(results[0]); // Assuming there's only one orders with the given ID
   });
 });
 
